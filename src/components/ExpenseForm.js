4f37ec058f6d590e278fc9f250de67c12c0dab43
Richardr123 <rwakos@gmail.com>
2018-01-07 14:45:12 -0500

    Adding Styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -55,9 +55,8 @@ export default class ExpenseForm extends React.Component {
 	};
 	render() {
 		return (
-			<div>
-				{this.state.error && <p>{this.state.error}</p>}
-				<form onSubmit={this.onSubmit}>
+				<form className='form' onSubmit={this.onSubmit}>
+					{this.state.error && <p className='form__error'>{this.state.error}</p>}
 					<input className='text-input'
 						type="text"
 						placeholder="Description"
@@ -71,7 +70,7 @@ export default class ExpenseForm extends React.Component {
 						value={this.state.amount}
 						onChange={this.onAmountChange}
 					/>
-					<SingleDatePicker className='text-input'
+					<SingleDatePicker
 						date={this.state.createdAt}
 						onDateChange={this.onDateChange}
 						focused={this.state.calendarFocused}
@@ -84,10 +83,12 @@ export default class ExpenseForm extends React.Component {
 						value={this.state.note}
 						onChange={this.onNoteChange}
 					>
-          </textarea>
-					<button>Add Expense</button>
+          			</textarea>
+					<div>
+						<button className='button'>Save Expense</button>
+					</div>
+
 				</form>
-			</div>
 		)
 	}
 }
